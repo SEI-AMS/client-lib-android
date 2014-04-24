8002c78e5a0096add1a640e65728bdd86c3c2cc5
James Root <jamesroot@gmail.com>
2014-04-24 14:39:38 -0400

    Added basic build system

---
+++ src/main/java/edu/cmu/sei/ams/cloudlet/Test.java
@@ -0,0 +1,14 @@
+package edu.cmu.sei.ams.cloudlet;
+
+/**
+ * User: jdroot
+ * Date: 4/24/14
+ * Time: 2:03 PM
+ */
+public class Test
+{
+    public static void main(String ... args)
+    {
+        Class c = CloudletFinder.class;
+    }
+}
