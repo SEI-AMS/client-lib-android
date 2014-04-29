3beae8dbac8e8030e6e3edb5cf792cc38d2bbf56
James Root <jamesroot@gmail.com>
2014-04-29 15:17:26 -0400

    Added a task to simplify finding a cloudlet and starting a service

---
+++ src/main/java/edu/cmu/sei/ams/cloudlet/android/FindCloudletAndStartService.java
@@ -0,0 +1,51 @@
+package edu.cmu.sei.ams.cloudlet.android;
+
+import android.content.Context;
+import edu.cmu.sei.ams.cloudlet.Cloudlet;
+import edu.cmu.sei.ams.cloudlet.Service;
+import edu.cmu.sei.ams.cloudlet.ServiceVM;
+import edu.cmu.sei.ams.cloudlet.rank.CloudletRanker;
+
+/**
+ * User: jdroot
+ * Date: 4/29/14
+ * Time: 2:55 PM
+ */
+public class FindCloudletAndStartService
+{
+    private Context mContext;
+    private String mServiceId;
+    private CloudletRanker mRanker;
+    private CloudletCallback<ServiceVM> mCallback;
+
+
+    public FindCloudletAndStartService(Context context, String serviceId, CloudletRanker ranker, CloudletCallback<ServiceVM> callback)
+    {
+        this.mContext = context;
+        this.mServiceId = serviceId;
+        this.mRanker = ranker;
+        this.mCallback = callback;
+    }
+
+    public void execute()
+    {
+        new FindCloudletByRankAsyncTask(mContext, mServiceId, mRanker, new CloudletCallback<Cloudlet>()
+        {
+            @Override
+            public void handle(Cloudlet result)
+            {
+                if (result == null)
+                    mCallback.handle(null);
+                try
+                {
+                    Service service = result.getServiceById(mServiceId); //This *should* never fail
+                    new StartServiceAsyncTask(service, mContext, mCallback);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }).execute();
+    }
+}
